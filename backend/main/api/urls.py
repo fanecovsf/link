85e4ba0f1b5ff9704043a0a317f46bd33978ed1f
fanecovsf <gustavo_h07@hotmail.com>
2023-09-26 23:06:00 -0300

    ✨ Feat: Implementado endpoint para logs

--- backend/main/api/urls.py
+++ backend/main/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from api.views import AutomationView, AutomationDetailView
+from api.views import AutomationView, AutomationDetailView, TaskLogView, TaskLogDetailView
 
 urlpatterns = [
-    path('automations/', AutomationView.as_view(), name='automation'),
-    path('automations/<id>', AutomationDetailView.as_view(), name='automation_detail')
+    path('automations/', AutomationView.as_view(), name='automations'),
+    path('automations/<id>', AutomationDetailView.as_view(), name='automations_detail'),
+    path('logs/', TaskLogView.as_view(), name='logs'),
+    path('logs/<id>', TaskLogDetailView.as_view(), name='logs_detail')
 ]
--- backend/main/api/views.py
+++ backend/main/api/views.py
@@ -4,65 +4,22 @@ from rest_framework.response import Response
 from api.services import AutomationServices, TaskLogServices
 from api.serializers import AutomationSerializer, TaskLogSerializer
 
-
-class AutomationView(APIView):
-
-
-    def get(self, request):
-        automations = AutomationServices.query_all()
-        serializer = AutomationSerializer(automations, many=True)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def post(self, request):
-        data = request.data
-        
-        if data:
-            serializer = AutomationSerializer(data=data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response(data={'error':'missing data'}, status=status.HTTP_400_BAD_REQUEST)
-        
-
-class AutomationDetailView(APIView):
+from main.abstracts import AbstractAPIView, AbstractDetailAPIView
 
 
-    def get(self, request, id):
-        try:
-            automation = AutomationServices.get(id)
-        except:
-            return Response(data={'error':'automation not found'}, status=status.HTTP_404_NOT_FOUND)
-        
-        if automation:
-            serializer = AutomationSerializer(automation)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        
-    def put(self, request, id):     
-        try:
-            automation = AutomationServices.get(id)
-        except:
-            return Response(data={'error':'automation not found'}, status=status.HTTP_404_NOT_FOUND)
-        
-        if automation:
-            serializer = AutomationSerializer(automation, data=request.data)
+class AutomationView(AbstractAPIView):
+    model_service = AutomationServices
+    model_serializer = AutomationSerializer
 
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            
-    def delete(self, request, id):
-        try:
-            automation = AutomationServices.get(id)
-        except:
-            return Response(data={'error':'automation not found'}, status=status.HTTP_404_NOT_FOUND)
-        
-        if automation:
-            automation.delete()
-            return Response(data={'deletion':'automation deleted'}, status=status.HTTP_204_NO_CONTENT)
+class AutomationDetailView(AbstractDetailAPIView):
+    model_service = AutomationServices
+    model_serializer = AutomationSerializer
+
+class TaskLogView(AbstractAPIView):
+    model_service = TaskLogServices
+    model_serializer = TaskLogSerializer
+
+class TaskLogDetailView(AbstractDetailAPIView):
+    model_service = TaskLogServices
+    model_serializer = TaskLogSerializer
         
